1073677b61fbfb9073f95cd9073132b4d6f12b0c
Tino <tino.muzambi@uct.ac.za>
2023-11-20 15:18:03 +0200

    Adds fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
 	content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		fontFamily: {
+			futuraStdBook: ["Futura Std Book", "sans-serif"],
+			futuraStdMedium: ["Futura Std Medium", "sans-serif"],
+		},
 		extend: {},
 	},
 	plugins: [],
