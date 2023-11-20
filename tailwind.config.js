6943d17efd928ed9fd4aa5d452add88e11f0cfee
Tino <tino.muzambi@uct.ac.za>
2023-11-20 15:29:42 +0200

    Removes fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@
 export default {
 	content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		fontFamily: {
-			futuraStdBook: ["Futura Std Book", "sans-serif"],
-			futuraStdMedium: ["Futura Std Medium", "sans-serif"],
-		},
 		extend: {},
 	},
 	plugins: [],
