789564224e998e45b1a4b05336b1cc5f0c7710f4
Tino <tino.muzambi@uct.ac.za>
2023-11-24 11:10:31 +0200

    Reverts to og design.

--- main.js
+++ main.js
@@ -31,46 +31,35 @@ function filterChart(e) {
 // This is the data used - https://github.com/bumbeishvili/sample-data/blob/main/data-oracle.csv
 d3.csv("./data/data.csv").then((data) => {
 	chart = new d3.OrgChart()
-		.nodeHeight((d) => 500)
-		.nodeWidth((d) => 300)
-		// .childrenMargin((d) => 50)
-		// .compactMarginBetween((d) => 60)
-		// .compactMarginPair((d) => 60)
-		// .neighbourMargin((a, b) => 60)
+		.nodeHeight((d) => 85 + 25)
+		.nodeWidth((d) => 220 + 2)
+		.childrenMargin((d) => 50)
+		.compactMarginBetween((d) => 35)
+		.compactMarginPair((d) => 30)
+		.neighbourMargin((a, b) => 20)
 		.nodeContent(function (d, i, arr, state) {
 			const color = "#FFFFFF";
 			const imageDiffVert = 25 + 2;
-			return `<div class="max-w-md shadow-2xl shadow-[#1479a7]">
-			<header
-				class="font-bold bg-[#e41619] text-white text-3xl text-center p-2"
-			>
-				${d.data.acronym}
-			</header>
-			<div class="bg-[#ecf0f6] pb-28">
-				<div class="p-2">
-					<p class="text-center text-lg">
-						${d.data.title}
-					</p>
-					<p class="text-center font-bold text-[#1479a7] text-lg pt-1">
-						${d.data.institution}
-					</p>
-					<p class="text-center text-[#1479a7] text-xl">${d.data.country}</p>
-				</div>
-			</div>
-			<footer class="bg-[#1479a7] relative">
-				<img
-					class="rounded-full max-w-full aspect-square h-48 border-[6px] border-white absolute top-[-80%] left-1/2 -translate-x-1/2"
-					src=${d.data.picture}
-					alt="PI Photo"
-				/>
-				
-				<p class="text-center text-white text-xl pt-24 pb-4">
-					<span class="font-bold">Contact PI: </span>${d.data.pi}
-				</p>
-			</footer>
-		</div>
-              
-                          `;
+			return ` <div style='width:${
+				d.width
+			}px;height:${d.height}px;padding-top:${imageDiffVert - 2}px;padding-left:1px;padding-right:1px'>
+					  <div style="font-family: 'Inter', sans-serif;background-color:${color};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: 1px solid #E4E2E9">
+						  <div style="display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${
+								d.data.id
+							}</div>
+						  <div style="background-color:${color};margin-top:${-imageDiffVert - 20}px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
+						  <div style="margin-top:${
+								-imageDiffVert - 20
+							}px;">   <img src=" ${d.data.picture}" style="margin-left:${20}px;border-radius:100px;width:40px;height:40px;" /></div>
+						  <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:10px">  ${
+								d.data.title
+							} </div>
+						  <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${
+								d.data.country
+							} </div>
+
+					  </div>
+				  </div>`;
 		})
 		.container(".chart-container")
 		.data(data)
